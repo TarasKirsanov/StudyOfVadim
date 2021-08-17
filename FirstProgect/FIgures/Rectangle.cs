b6d8bd31e737b2988a3c00110d3455b4260b901f
kirsanovtaras8@gmail.com <kirsanovtaras8@gmail.com>
2021-08-17 22:10:49 +0300

    First commmit

---
+++ FirstProgect/FIgures/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstProgect.FIgures
+{
+    public class Rectangle
+    {
+        //Поле
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        //конструктор
+        public Rectangle()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+
+        //консруктор
+        public Rectangle(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        //Метод
+        public int GetArea()
+        {
+            var area = X * Y;
+            return area;
+        }
+
+        //создать метод для получения периметра 
+
+        // создать метод нахождения диагонали float 
+
+        //создать конструктор в который приходит одно значени и обе стороны становяться равными этому значению
+    }
+}
---
+++ FirstProgect/Program.cs
@@ -0,0 +1,17 @@
+﻿using FirstProgect.FIgures;
+using System;
+
+namespace FirstProgect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // создать класс треугольник и в нем реализовать 2 конструктора первый без парамтров () второй с 3 параметрами (x,y,z), а также 2 метода: получение площади и получение периметра
+            Rectangle squard = new Rectangle(1,1);
+            Console.WriteLine(squard.GetArea());
+            Console.ReadKey();
+        }
+    }
+
+}
